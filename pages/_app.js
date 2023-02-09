32777c30d7b91854d1e13a00bef48ae25dddba4b
Winnard Arthur <winnardarthur7@gmail.com>
2023-02-09 17:52:29 +0000

    wrap app in SessionProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '@/styles/globals.css'
 import { StoreProvider } from '@/utils/Store'
+import { SessionProvider } from 'next-auth/react'
 
-export default function App({ Component, pageProps }) {
-  return <StoreProvider>
-     <Component {...pageProps} /> 
-  </StoreProvider>
+
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <StoreProvider>
+        <Component {...pageProps} /> 
+      </StoreProvider>
+    </SessionProvider> 
+  )
 }
