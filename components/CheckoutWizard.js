a67b9589a6f11d62911baf00a316d3147af92f85
Winnard Arthur <winnardarthur7@gmail.com>
2023-02-10 11:05:15 +0000

    increase border bottom width

--- components/CheckoutWizard.js
+++ components/CheckoutWizard.js
@@ -6,7 +6,7 @@ export default function CheckoutWizard({ activeStep = 0 }) {
         {
             ['User Login', 'Shipping Address', 'Payment Method', 'Place Order'].map((step, index) => (
                 <div key={step} className={
-                    `flex-1 border-b-2 text-center ${index <= activeStep ? 'border-indigo-500 text-indigo-500' : 'border-gray-400 text-gray-400' }`
+                    `flex-1 border-b-4 text-center ${index <= activeStep ? 'border-indigo-500 text-indigo-500' : 'border-gray-400 text-gray-400' }`
                 }>{step}</div>
             ))
         }
