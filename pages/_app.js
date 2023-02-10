d24b42d87cfa6dd469261b698fe86c01c57cba52
Winnard Arthur <winnardarthur7@gmail.com>
2023-02-10 10:32:51 +0000

    add Auth higher order component for authentication

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,36 @@
 import '@/styles/globals.css'
 import { StoreProvider } from '@/utils/Store'
-import { SessionProvider } from 'next-auth/react'
+import { SessionProvider, useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <StoreProvider>
+        {Component.auth ? (
+          <Auth>
+            <Component {...pageProps} /> 
+          </Auth>
+        )
+        :
         <Component {...pageProps} /> 
+        }
       </StoreProvider>
     </SessionProvider> 
   )
+}
+
+function Auth({ children }) {
+  const router = useRouter();
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push('/unauthorized?message=Login required')
+    }
+  });
+
+  if (status === 'loading') return <div>Loading...</div>
+
+  return children;
 }
