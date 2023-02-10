06205c816aa7703d813d323237235b4192dfdf6e
Winnard Arthur <winnardarthur7@gmail.com>
2023-02-10 10:34:09 +0000

    add case for saving shipping

--- utils/Store.js
+++ utils/Store.js
@@ -5,7 +5,7 @@ export const Store = createContext();
 
 const initialState = {
     cart: Cookies.get('cart') ? JSON.parse(Cookies.get('cart')) : 
-        { cartItems: [] }
+        { cartItems: [], shippingAddress: {} }
 }
 
 function reducer(state, action) {
@@ -40,6 +40,17 @@ function reducer(state, action) {
                 }
             }
         }
+        case 'SAVE_SHIPPING_ADDRESS': 
+            return {
+                ...state,
+                cart: {
+                    ...state.cart,
+                    shippingAddress: {
+                        ...state.cart.shippingAddress,
+                        ...action.payload 
+                    }
+                }
+            }
         default:
             return state;
     }
