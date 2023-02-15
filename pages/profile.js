38a4f0de7494f97d5497b905361e5b90f4fad566
Winnard Arthur <winnardarthur7@gmail.com>
2023-02-15 22:36:01 +0000

    add profile screen

---
+++ pages/profile.js
@@ -0,0 +1,116 @@
+import Layout from '@/components/Layout'
+import { getError } from '@/utils/error';
+import axios from 'axios';
+import { signIn, useSession } from 'next-auth/react'
+import React, { useEffect } from 'react'
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+
+
+export default function ProfileScreen() {
+    const { data: session } = useSession();
+
+    const { 
+        handleSubmit,
+        register,
+        getValues,
+        setValue,
+        formState: { errors }
+    } = useForm();
+
+    useEffect(() => {
+        setValue('name', session.user.name);
+        setValue('email', session.user.email)
+    }, [session.user, setValue]);
+
+    const submitHandler = async ({ name, email, password, confirmPassword }) => {
+        try {
+            await axios.put(`/api/auth/update`, { 
+                name, email, password 
+            });
+
+            const result = await signIn('credentials', {
+                redirect: false,
+                email, 
+                password 
+            })
+
+            toast.success('Profile updated successfully');
+
+            if(result.error) {
+                toast.error(result.error);
+            }
+        } catch (error) {
+            toast.error(getError(error))
+        }
+    }
+
+  return (
+    <Layout title="Profile">
+        <form className='mx-auto max-w-screen-md' onSubmit={handleSubmit(submitHandler)}>
+            <h1 className='mb-4 text-xl'>Update Profile</h1>
+
+            <div className='mb-4'>
+                <label htmlFor="name">Name</label>
+                <input type={"text"} className='w-full' id='name' autoFocus 
+                    {...register('name', {
+                        required: 'Please enter name'
+                    })}
+                />
+                {errors.name && <div className='text-red-500'>{errors.name.message}</div>}
+            </div>
+
+            <div className='mb-4'>
+                <label htmlFor="email">Email</label>
+                <input type={"email"} className='w-full' id='email' 
+                    {...register('email', {
+                        required: 'Please enter email',
+                        value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                        message: 'Please enter valid email'
+
+                    })}
+                />
+                {errors.email && <div className='text-red-500'>{errors.email.message}</div>}
+            </div>
+
+            <div className='mb-4'>
+                <label htmlFor="password">Password</label>
+                <input type={"password"} className='w-full' id='password' 
+                    {...register('password', {
+                        required: 'Please enter password',
+                        minLength: { value: 6, message: 'Password should be more than 5 characters'}
+
+                    })}
+                />
+                {errors.password && <div className='text-red-500'>{errors.password.message}</div>}
+            </div>
+
+            <div className='mb-4'>
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input type={"confirmPassword"} className='w-full' id='confirmPassword' 
+                    {...register('confirmPassword', {
+                        required: 'Please confirm password',
+                        validate: (value) => value === getValues('password'),
+                        minLength: {value: 6, message: 'Confirm password should be more than 6 characters'}
+                    })}
+                />
+                {errors.confirmPassword && (
+                    <div className='text-red-500'>
+                        {errors.confirmPassword.message}
+                    </div>
+                )}
+                {errors.confirmPassword && errors.confirmPassword.type === 'validate' && (
+                    <div className='text-red-500'>Password do not match</div>
+                )}
+            </div>
+
+            <div className='mb-4'>
+                <button className='primary-button'>Update Profile</button>
+            </div>
+        </form>
+    </Layout>
+  )
+}
+
+
+ProfileScreen.auth = true;
